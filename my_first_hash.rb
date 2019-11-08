ba5e3244310d20480e022dd2a0af3decf0dad31c
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-08 18:37:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = { "dream_car" => "'69 Bel"
+  my_hash = { "dream_car" => "'54 Bel Air" }
   
 end
 
